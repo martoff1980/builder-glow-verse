1ae1390551ee8b3eb72ed14891254141c82fcb8f
martoff <martoff@ukr.net>
2025-10-27 18:31:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,12 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     fs: {
-      allow: ["./client", "./shared"],
+      //allow: ["./client", "./shared"],
+      allow: [
+        path.resolve(__dirname, "./client"),
+        path.resolve(__dirname, "./shared"),
+        path.resolve(__dirname),
+      ],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
